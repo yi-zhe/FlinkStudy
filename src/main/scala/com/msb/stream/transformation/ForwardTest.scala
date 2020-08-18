c3012ef50c4d5fc419854cdf490ed14ae298646e
liuyang <liuyang@coohua.com>
2020-08-18 08:30:57 +0800

    Forward

--- src/main/scala/com/msb/stream/transformation/ForwardTest.scala
+++ src/main/scala/com/msb/stream/transformation/ForwardTest.scala
@@ -4,14 +4,17 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
 /**
- * 下游的分区会接收到上游所有分区的数据
+ * 上游分区与下游分区一一对应的分发,
+ * 上下游分区数必须一致
  */
-object BroadcastTest {
+object ForwardTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val stream = env.generateSequence(1, 10).setParallelism(2)
     stream.writeAsText("./data/stream1").setParallelism(2)
-    stream.broadcast.writeAsText("./data/stream2").setParallelism(4)
+    // 不允许改变并行度
+//    stream.forward.writeAsText("./data/stream2").setParallelism(4)
+    stream.forward.writeAsText("./data/stream2").setParallelism(2)
     env.execute()
   }
 }
